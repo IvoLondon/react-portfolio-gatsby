3095773666d0315fc4c97b0b8759de712d8eb094
Ivo <ivelin.iliev@dusted.com>
2018-12-04 15:49:18 +0000

    Add hidden field form-name

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,8 @@ const ContactPage = () => (
 
   <Layout>
     <h1>Get in touch</h1>
-   	<form name="contact" method="post" action="/success" data-netlify="true">>
+   	<form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>Your Name: <input type="text" name="name" /></label>   
       </p>
