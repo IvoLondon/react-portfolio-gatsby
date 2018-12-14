21386f79c3a2e6f9b0d7d936a939677c8f96f8a2
Iwi4a <ivelin.iliev@hotmail.com>
2018-12-14 02:15:12 +0000

    Contact page -> content position

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,18 +12,15 @@ const ContactPage = () => (
       <p>Don't be shy, drop me a message and say hi!</p>
      	<form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact" />
-        <p>
+
           <input placeHolder="Your Name" type="text" name="name" />   
-        </p>
-        <p>
+
           <input placeHolder="Your Email" type="email" name="email" />
-        </p>
-        <p>
+
           <textarea placeHolder="Message" name="message"></textarea>
-        </p>
-        <p>
+
           <button className="button-one" type="submit">Send</button>
-        </p>
+
       </form>
     </div>
   </Layout>
